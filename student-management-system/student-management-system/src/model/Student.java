5b1af06a86b322daa9c223bc0e142452f63ef582
system sylvere BARINDA <systembarinda@gmail.com>
2025-12-12 14:36:54 +0200

    chore:done to develop Student.java

--- student-management-system/student-management-system/src/model/Student.java
+++ student-management-system/student-management-system/src/model/Student.java
@@ -1,7 +1,32 @@
 package model;
 
 public class Student {
-    private int id;
+    private Integer id;
     private String firstName;
     private String lastName;
+    private String email;
+    private Integer age;
+
+    public Student(Integer id, String firstName, String lastName, String email, Integer age) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.age = age;
+    }
+    public Student(String firstName, String lastName, String email, Integer age) {
+        this(null, firstName, lastName, email, age);
+    }
+
+    // getters & setters
+    public Integer getId() { return id; }
+    public void setId(Integer id) { this.id = id; }
+    public String getFirstName() { return firstName; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+    public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    public Integer getAge() { return age; }
+    public void setAge(Integer age) { this.age = age; }
 }
