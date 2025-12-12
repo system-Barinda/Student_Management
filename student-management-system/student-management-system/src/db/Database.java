d93e70a6e013265210c23baf54855c95a78c9c4b
system sylvere BARINDA <systembarinda@gmail.com>
2025-12-12 15:08:55 +0200

    feat(fix the mysql connector

--- student-management-system/student-management-system/src/db/Database.java
+++ student-management-system/student-management-system/src/db/Database.java
@@ -5,17 +5,20 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Database {
-    private static final String URL = "jdbc:sqlite:students.db"; // file DB
+
+    private static final String URL = "jdbc:mysql://localhost:3306/student_management?useSSL=false&serverTimezone=UTC";
+    private static final String USER = "root"; 
+    private static final String PASS = ""; 
 
     static {
         try {
-            Class.forName("org.sqlite.JDBC");
+            Class.forName("com.mysql.cj.jdbc.Driver"); // MySQL driver
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
     }
 
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL);
+        return DriverManager.getConnection(URL, USER, PASS);
     }
 }
